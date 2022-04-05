3b2ac9c04207203d2d85188dbf0676e71875c746
Alex <amongus@gmail.com>
2022-04-05 21:15:26 +0300

    Задания 1-5

--- begining.py
+++ begining.py
@@ -1,16 +1,18 @@
-# Задача 1
+#Задача 1
 def task1():
     print('Alexey');
     print('Адрес не скажу!!!');
 
 task1();
 
+#Задача 2
 def task2():
     name = input('Введите ваше имя\n')
     print(f"Желаю здравствовать, {name}.");
 
 task2();
 
+#Задача 3
 def task3():
     width = float(input("Какая ширина комнаты\n"));
     length = float(input('Какая длина комнаты\n'));
@@ -19,6 +21,7 @@ def task3():
 
 task3()
 
+#Задача 4
 def task4():
     width = float(input("Какая ширина участка\n"));
     length = float(input('Какая длина участка\n'));
@@ -27,3 +30,12 @@ def task4():
 
 task4();
 
+#Задача 5
+def task5():
+    lessALitter = float(input("Сколько бутылок объемом 1 литр и меньше\n"));
+    moreThanALitter =float(input("Сколько бутылок объемом больше литра\n"));
+    sum = float(lessALitter * 0.1 + moreThanALitter * 0.25);
+    sum = round(sum, 2);
+    print(f"Вы сдали бутылок на сумму {sum} $")
+
+task5();
