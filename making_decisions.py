3843a78117f5c8d9c566e6b4752ab60f2c26fa2a
Alex <amongus@gmail.com>
2022-04-18 19:39:35 +0300

    Задание 2_13

--- making_decisions.py
+++ making_decisions.py
@@ -182,16 +182,46 @@
 
 # task11();
 
-# Задание 12
-def task12():
-    year = int(input('Введите год\n'));
-    if year % 400 == 0:
-        print('Год високосный');
-    elif year % 100 == 0:
-        print('Год не високосный');
-    elif year % 4 == 0:
-        print('Год високосный');
-    else:
-        print('Год не високосный');
+# # Задание 12
+# def task12():
+#     year = int(input('Введите год\n'));
+#     if year % 400 == 0:
+#         print('Год високосный');
+#     elif year % 100 == 0:
+#         print('Год не високосный');
+#     elif year % 4 == 0:
+#         print('Год високосный');
+#     else:
+#         print('Год не високосный');
 
-task12();
+# task12();
+
+#Задание 13
+def task13():
+    import random;
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, '00', '0']
+    number = random.choice(list)
+    print(f"Выпавший номер {number}...");
+    print(f"Выигравшая ставка {number}");
+    if number == '00':
+        print('Выпавший номер: 00')
+    elif number == '0':
+        print('Выпавший номер: 0')
+    else:
+        if (number == '1' or number == '3' or  number == '5' or number == '7' or  number == '9' or number == '12' or  number == '14' or number == '16' or number == '18' or number == '19' or number == '21' or number == '23' or  number == '25' or number == '27' or  number == '30' or number == '32' or number == '34' or number == '36'):
+            print('Выигравшая ставка: красное');
+        elif (number == '2' or number == '4' or  number == '6' or number == '8' or  number == '10' or number == '11' or  number == '13' or number == '15' or number == '17' or number == '20' or number == '22' or number == '24' or  number == '26' or number == '28' or  number == '29' or number == '31' or number == '33' or number == '35'):
+            print('Выигравшая ставка: черное');
+        
+        if int(number) % 2 == 1: 
+            print('Выигравшая ставка: нечетное');
+        else:
+            print('Выигравшая ставка: четное')
+
+        if int(number) > 0 and int(number) < 19:
+            print('Выигравшая ставка: от 1 до 18');
+        else: 
+            print('Выигравшая ставка: от 19 до 36');
+
+task13();
+   
